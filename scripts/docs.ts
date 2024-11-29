ed1e2c5d526cc9d12c269bd461f935b076809788
Robert Brightline <robert.brightline@gmail.com>
2024-11-29 15:00:12 -0600

    [env] [chore] version fix

---
+++ scripts/docs.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env ts-node
+
+// compodoc -p tsconfig.doc.json
+
+import q from 'inquirer';
+import { libs } from './constant/libs';
+import { execSync } from 'child_process';
+import { chdir } from 'process';
+import { join } from 'path';
+
+q.prompt([
+  {
+    name: 'lib',
+    type: 'list',
+    message: 'Pick library',
+    choices: libs(),
+  },
+]).then(({ lib }) => {
+  chdir(join(__dirname, '..', 'libs', lib));
+  execSync(`compodoc -p tsconfig.json -d ../../docs/docs/${lib}`);
+});
