91e0faa7f7584ffb6b1af3c05f062a6f4d03a912
Robert Brightline <robert.brightline@gmail.com>
2024-12-07 12:37:44 -0600

    [schemas] [chore] created the model schemas

--- libs/json/src/lib/json.spec.ts
+++
@@ -1,7 +0,0 @@
-import { json } from './json';
-
-describe('json', () => {
-  it('should work', () => {
-    expect(json()).toEqual('json');
-  });
-});
--- libs/json/src/lib/json.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-export function json(jsonFilePath: string): string {
-  const root = join('..', jsonFilePath);
-  const content = readFileSync(jsonFilePath);
-  
-  return 'json';
-}
